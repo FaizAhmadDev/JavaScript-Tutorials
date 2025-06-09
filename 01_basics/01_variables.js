050a6958ca18ddb81ebed1d6da29ffc1184102ae
Faiz Ahmad <faizahmad100001@gmail.com>
2025-06-09 05:41:41 +0000

    changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,6 @@
 const id = 122
 let email = "faiz@example.com"
-var password = "abc12"
+var pass = "abc12"
 accountCity = "here"
 let accountState; // Undefined if not initialized
 
@@ -11,9 +11,9 @@ let accountState; // Undefined if not initialized
 
 // id = 100 // const variables cannot be changed
 email = "fa@fa.com"
-password = "121212"
+pass = "121212"
 accountCity = "not here"
 
 console.log(id)
 
-console.table([id, email, password, accountCity, accountState])
+console.table([id, email, pass, accountCity, accountState])
