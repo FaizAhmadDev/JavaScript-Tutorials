16c258bbc111bab7fdf96c95661bd0107c96cd58
Faiz Ahmad <faizahmad100001@gmail.com>
2025-06-09 06:08:05 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";  // treat all JS code as newer version 
+
+// alert(3+4)       // this is node env not JS env(browser)
+// name = "hdjh";   // will not work
+
+console.log( "Data " 
+    +
+    "Types"
+  ) // code readability is important
+
+let name = "fa"
+let age = 18
+let isLoggedin = false
+const msg = `Hi its my "Javascript tutorials", hope you 'learned' something today`
+
+console.log(msg)
+
+// number => 2 ^ 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => uninitialized variables
+// symbol
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+console.log(typeof msg) // undefined
+
+
