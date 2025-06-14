c7f18191463525ebfc122161a9b84c80a37d4c82
Faiz Ahmad <faizahmad100001@gmail.com>
2025-06-14 14:11:14 +0000

    type conversion

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,19 +1,15 @@
 "use strict";  // treat all JS code as newer version 
 
 // alert(3+4)       // this is node env not JS env(browser)
-// name = "hdjh";   // will not work
+// name = "hdjh";   // will not work when using strict mode
 
-console.log( "Data " 
+console.log("Topic: Data " 
     +
-    "Types"
-  ) // code readability is important
+    "Types\n"
+  )                 // code readability is important
 
-let name = "fa"
-let age = 18
-let isLoggedin = false
-const msg = `Hi its my "Javascript tutorials", hope you 'learned' something today`
 
-console.log(msg)
+// -----List Of Commom DataTypes-----
 
 // number => 2 ^ 53
 // bigint
@@ -25,8 +21,30 @@ console.log(msg)
 
 // object
 
-console.log(typeof undefined) // undefined
-console.log(typeof null) // object
-console.log(typeof msg) // undefined
 
+const a = 42;
+const b = "FA";
+const c = true;
+const d = { key: "value" };
+const e = [1, 2, 3];
+const f = null;
+const g = undefined;
+const h = function () {};
+const i = Symbol("sym");
 
+const variables = [
+  { name: "a", value: a, type: typeof a },
+  { name: "b", value: b, type: typeof b },
+  { name: "c", value: c, type: typeof c },
+  { name: "d", value: d, type: typeof d },
+  { name: "e", value: e, type: typeof e },
+  { name: "f", value: f, type: typeof f },
+  { name: "g", value: g, type: typeof g },
+  { name: "h", value: h, type: typeof h },
+  { name: "i", value: i, type: typeof i },
+];
+
+console.table(variables);
+
+const msg = `\n Hi its my "Javascript tutorials", hope you 'learned' something :)`
+console.log(msg)
